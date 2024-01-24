d402f3a381ecd6507e77796e7b0118bc38a475a2
mustafa-kamel <mostafak252@gmail.com>
2024-01-25 00:50:17 +0200

    Add swagger documentation using drf-spectacular

--- pharmacies/settings.py
+++ pharmacies/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     "django.contrib.staticfiles",
     "rest_framework",
     "pharmacy",
+    "drf_spectacular",
 ]
 
 MIDDLEWARE = [
@@ -129,4 +130,12 @@ REST_FRAMEWORK = {
     "DEFAULT_AUTHENTICATION_CLASSES": [
         "rest_framework.authentication.BasicAuthentication",
     ],
+    "DEFAULT_SCHEMA_CLASS": "drf_spectacular.openapi.AutoSchema",
+}
+
+SPECTACULAR_SETTINGS = {
+    "TITLE": "Your Project API",
+    "DESCRIPTION": "Your project description",
+    "VERSION": "1.0.0",
+    "SERVE_INCLUDE_SCHEMA": False,
 }
